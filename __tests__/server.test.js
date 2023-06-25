fa5258f3660c91fc60dd449f42bb938e1481dd5f
JacvaSipt <steamcow12@hotmail.com>
2023-06-25 14:11:44 +0100

    create a server test for '/' to expect 200 with 'text/html'

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -1,2 +1,11 @@
 const request = require("supertest");
 const app = require("../server");
+
+describe("GET /", () => {
+  test("Should respond with 200 status and type text/html", () => {
+    return request(app)
+      .get("/")
+      .expect(200)
+      .then(({ type }) => expect(type).toBe("text/html"));
+  });
+});
