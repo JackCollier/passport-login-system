bb80b3bc4e7c98003d7d8a9b8e03fad34c48e98a
JacvaSipt <steamcow12@hotmail.com>
2023-06-25 14:18:00 +0100

    create testing for /login and /register

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -9,3 +9,21 @@ describe("GET /", () => {
       .then(({ type }) => expect(type).toBe("text/html"));
   });
 });
+
+describe("GET /login", () => {
+  test("Should respond with 200 status and type text/html", () => {
+    return request(app)
+      .get("/login")
+      .expect(200)
+      .then(({ type }) => expect(type).toBe("text/html"));
+  });
+});
+
+describe("GET /register", () => {
+  test("Should respond with 200 status and type text/html", () => {
+    return request(app)
+      .get("/login")
+      .expect(200)
+      .then(({ type }) => expect(type).toBe("text/html"));
+  });
+});
