353fc8afcf3229a4c1d2e391a0cca9ae971f2ecb
JacvaSipt <steamcow12@hotmail.com>
2023-06-25 14:16:31 +0100

    create controllers for login and register page

--- controllers/server-controllers.js
+++ controllers/server-controllers.js
@@ -1,3 +1,11 @@
 exports.getStaticPage = (req, res) => {
   res.render("index.ejs");
 };
+
+exports.getLogin = (req, res) => {
+  res.render("login.ejs");
+};
+
+exports.getRegister = (req, res) => {
+  res.render("register.ejs");
+};
