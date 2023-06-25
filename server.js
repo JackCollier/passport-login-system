805d6227ee7f6d74de86b8af3c475dd8b27521fc
JacvaSipt <steamcow12@hotmail.com>
2023-06-25 14:05:28 +0100

    export app

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require("express");
 const { getStaticPage } = require("./controllers/server-controllers");
 const app = express();
 
+app.set("view-engine", "ejs");
+
 app.get("/", getStaticPage);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
 });
+
+module.exports = app;
